74c88c1e36c86cd894d5e3783cce1708aee83d6f
MoNaBOSS <moniruliislam1066@gmail.com>
2025-12-24 23:39:11 +0600

    Update

--- pages/standard.tsx
+++ pages/standard.tsx
@@ -1,10 +1,18 @@
 import StakePage from "../components/StakePage";
+import {
+  STAKING_POOL_ADDRESS,
+  STANDARD_NFT_ADDRESS,
+  TOKEN_ADDRESS,
+} from "../constants/config";
 
 export default function StandardStake() {
   return (
     <StakePage
       pageName="Standard"
-      collectionAddress={process.env.NEXT_PUBLIC_STANDARD_NFT_COLLECTION!}
+      nftAddress={STANDARD_NFT_ADDRESS}
+      planIndex={1}
+      stakingAddress={STAKING_POOL_ADDRESS}
+      tokenAddress={TOKEN_ADDRESS}
     />
   );
 }
